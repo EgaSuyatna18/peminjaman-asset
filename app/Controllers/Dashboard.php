b48fb3978a243e767be5f44c1ce1facf38e0ba7d
unknown <egasuyatna18@gmail.com>
2024-05-12 18:36:16 +0700

    finished

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -10,7 +10,14 @@ class Dashboard extends BaseController
     function index() {
         if(!self::isLoggedIn()) return redirect()->to('/')->with('error', 'Anda Belum Login!');
         $title = session()->get('userdata')->role . ' | Dashboard';
-        return view('dashboard/index', compact('title'));
+        $user = new \App\Models\UserModel();
+        $petugasBMN = $user->where('role', 'Petugas BMN')->countAllResults();
+        $ketuaBagianUmum = $user->where('role', 'Ketua Bagian Umum')->countAllResults();
+        $pengurus = $user->where('role', 'Pengurus Peralatan Pengeboran / Survei Explorasi')->countAllResults();
+        $ketuaTimKelompokKerja = $user->where('role', 'Ketua Tim Kelompok Kerja')->countAllResults();
+        $kepalaPSDMBP = $user->where('role', 'Kepala PSDMBP')->countAllResults();
+
+        return view('dashboard/index', compact('title', 'petugasBMN', 'ketuaBagianUmum', 'pengurus', 'ketuaTimKelompokKerja', 'kepalaPSDMBP'));
     }
 
     function isLoggedIn() {
--- app/Views/dashboard/index.php
+++ app/Views/dashboard/index.php
@@ -1,4 +1,97 @@
 <?= $this->extend('layouts/dashboardMaster') ?>
 <?= $this->section('main') ?>
+<!-- Content Row -->
+<div class="row">
 
+<!-- Earnings (Monthly) Card Example -->
+<div class="col-xl-2 offset-1 col-md-12 mb-4">
+    <div class="card border-left-primary shadow h-100 py-2">
+        <div class="card-body">
+            <div class="row no-gutters align-items-center">
+                <div class="col mr-2">
+                    <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                        Petugas BMN</div>
+                    <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $petugasBMN ?></div>
+                </div>
+                <div class="col-auto">
+                    <i class="fas fa-user-tie fa-2x text-gray-300"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Earnings (Monthly) Card Example -->
+<div class="col-xl-2 col-md-12 mb-4">
+    <div class="card border-left-success shadow h-100 py-2">
+        <div class="card-body">
+            <div class="row no-gutters align-items-center">
+                <div class="col mr-2">
+                    <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                        Ketua Bagian Umum</div>
+                    <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $ketuaBagianUmum ?></div>
+                </div>
+                <div class="col-auto">
+                    <i class="fas fa-user-tie fa-2x text-gray-300"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Earnings (Monthly) Card Example -->
+<div class="col-xl-2 col-md-12 mb-4">
+    <div class="card border-left-info shadow h-100 py-2">
+        <div class="card-body">
+            <div class="row no-gutters align-items-center">
+                <div class="col mr-2">
+                    <div class="text-xs font-weight-bold text-info text-uppercase mb-1">
+                        Pengurus Peralatan Pengeboran / Survei Explorasi</div>
+                    <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $pengurus ?></div>
+                </div>
+                <div class="col-auto">
+                    <i class="fas fa-user-tie fa-2x text-gray-300"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Pending Requests Card Example -->
+<div class="col-xl-2 col-md-12 mb-4">
+    <div class="card border-left-warning shadow h-100 py-2">
+        <div class="card-body">
+            <div class="row no-gutters align-items-center">
+                <div class="col mr-2">
+                    <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
+                    Ketua Tim Kelompok Kerja</div>
+                    <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $ketuaTimKelompokKerja ?></div>
+                </div>
+                <div class="col-auto">
+                    <i class="fas fa-user-tie fa-2x text-gray-300"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="col-xl-2 col-md-12 mb-4">
+    <div class="card border-left-danger shadow h-100 py-2">
+        <div class="card-body">
+            <div class="row no-gutters align-items-center">
+                <div class="col mr-2">
+                    <div class="text-xs font-weight-bold text-danger text-uppercase mb-1">
+                        Kepala PSDMBP</div>
+                    <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $kepalaPSDMBP ?></div>
+                </div>
+                <div class="col-auto">
+                    <i class="fas fa-user-tie fa-2x text-gray-300"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+
+<!-- Content Row -->
 <?= $this->endSection() ?>
